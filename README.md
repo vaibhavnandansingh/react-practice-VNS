005aa29d910f20cabf8d42dc5854bd6b2c4947e7
vaibhavnandansingh <38402415+vaibhavnandansingh@users.noreply.github.com>
2022-12-14 23:13:35 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-practice-VNS
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-c8atkr)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Let's crack react</h1>
+      <p>I am back bro</p>
+    </div>
+  );
+}
